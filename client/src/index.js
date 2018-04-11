0c221fde0929f1aa64e5d36d58eadf5d93e4a0c6
nasturtus <nasturtus@gmail.com>
2018-04-11 11:58:47 +0800

    [Santosh/Victor][#46] Refactor index and Forgot (css files), create Common.css

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,12 @@ import "whatwg-fetch";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 // import "./index.css";
+import "./Common.css";
 import "./components/Footer.css";
 import "./components/Header.css";
 import "./components/Home.css";
 import "./components/Account/Login.css";
+import "./components/Account/Forgot.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
