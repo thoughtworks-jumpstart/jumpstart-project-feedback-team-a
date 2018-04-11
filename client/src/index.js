e7412571df3307bc455366ea11706a1234080e89
nasturtus <nasturtus@gmail.com>
2018-04-11 10:45:33 +0800

    [Santosh/Victor][#46] Refactor Login Footer Home index (all css files)

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,11 @@ import registerServiceWorker from "./registerServiceWorker";
 import "whatwg-fetch";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
-import "./index.css";
+// import "./index.css";
+import "./components/Footer.css";
+import "./components/Header.css";
+import "./components/Home.css";
+import "./components/Account/Login.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
