d1b330ab2e36317dfd4919e1bc5cb35ed576b86a
mohafizz <mohafizz@gmail.com>
2018-04-17 06:23:33 +0800

    [Hafiz,Santosh][#25] added snapshot test which still have warning failures

--- client/src/components/Account/Signup/WrappedSignup.test.js
+++ client/src/components/Account/Signup/WrappedSignup.test.js
@@ -1,12 +1,18 @@
 import { WrappedSignup } from "./WrappedSignup";
 import React from "react";
 import { shallow } from "enzyme";
+import { Cookies } from "react-cookie";
+import { instanceOf } from "prop-types";
 
 describe("WrappedSignup", () => {
   it("should should have the following structure", () => {
-    const props = {};
+    const props = {
+      history: {},
+      cookies: instanceOf(Cookies),
+      messageContext: { messages: {} },
+      sessionContext: { session: {} }
+    };
     const wrapper = shallow(<WrappedSignup {...props} />);
     expect(wrapper).toMatchSnapshot();
-    console.log(wrapper.prop("handleChange"));
   });
 });
