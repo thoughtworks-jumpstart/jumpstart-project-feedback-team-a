57185d26b47407339041efad5f9c24c92920d6fe
yaminmhd <mdyamin.drzee@gmail.com>
2018-04-25 15:32:52 +0800

    [Ben/Yamin][#40] Fixed WrappedApp.test.js by including setItem function in the mockSessionStorage and added setMaxListeners(0) in the jest setupTest.js to fix possible EventEmitter memory leak detected

--- client/src/components/App/WrappedApp.test.js
+++ client/src/components/App/WrappedApp.test.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import WrappedApp from "./WrappedApp";
 
 it("renders without crashing", () => {
-  // const div = document.createElement("div");
-  // const mockDocument = { cookie: "token=123" };
-  // const mockSessionStorage = { getItem: () => JSON.stringify({}) };
-  // ReactDOM.render(
-  //   <WrappedApp document={mockDocument} sessionStorage={mockSessionStorage} />,
-  //   div
-  // );
-  // ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement("div");
+  const mockDocument = { cookie: "token=123" };
+  const mockSessionStorage = {
+    getItem: () => JSON.stringify({}),
+    setItem: () => {}
+  };
+  ReactDOM.render(
+    <WrappedApp document={mockDocument} sessionStorage={mockSessionStorage} />,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,5 +3,5 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
-
+process.setMaxListeners(0);
 process.on("unhandledRejection", r => console.log(r));
