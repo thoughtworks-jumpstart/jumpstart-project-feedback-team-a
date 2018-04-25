91614575c804179f82f89f26d02989d0236ff144
nasturtus <nasturtus@gmail.com>
2018-04-25 10:45:11 +0800

    [Hafiz/Santosh] Rectify setItem undefined bug in App.test.js

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -7,15 +7,15 @@ import * as actions from "../../actions/auth";
 describe("App", () => {
   let props, wrapper;
   beforeEach(() => {
-    global.sessionStorage = {
-      setItem: jest.fn()
-    };
     props = {
       cookies: {
         token: "123",
         get: jest.fn()
       },
-      sessionContext: { saveSession: jest.fn() }
+      sessionContext: { saveSession: jest.fn() },
+      sessionStorage: {
+        setItem: jest.fn()
+      }
     };
     wrapper = shallow(<App {...props} />);
   });
@@ -26,13 +26,6 @@ describe("App", () => {
 
   it("should fetch user and set jwtToken and user", async () => {
     actions.getCurrentUser = jest.fn();
-    const props = {
-      cookies: {
-        token: "123",
-        get: jest.fn()
-      },
-      sessionContext: { saveSession: jest.fn() }
-    };
     wrapper = shallow(<App {...props} />);
     expect(actions.getCurrentUser).toHaveBeenCalledTimes(1);
   });
