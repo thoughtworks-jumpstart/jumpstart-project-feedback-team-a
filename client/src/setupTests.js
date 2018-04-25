d881b5babfa9732ee9007963790c1ed8eed57bf5
nasturtus <nasturtus@gmail.com>
2018-04-25 10:26:11 +0800

    [All] Add global setup to show stack trace for UnhandledPromiseRejection

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,5 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+process.on("unhandledRejection", r => console.log(r));
