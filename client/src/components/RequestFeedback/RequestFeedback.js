0b66b54ba49fd361083010827e745269dfb9e76e
Benjamin Ang <benjamin.ang@gmail.com>
2018-04-26 16:30:13 +0800

    [Ben] Fixed the merge conflict for requestFeedback.js

--- client/src/components/RequestFeedback/RequestFeedback.js
+++ client/src/components/RequestFeedback/RequestFeedback.js
@@ -37,71 +37,7 @@ class RequestFeedback extends React.Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    if (
-      window.confirm(
-        `Please confirm if you would like to send this feedback to the following recipient: ${
-          this.state.email
-        }?`
-      )
-    ) {
-      await this.setState({ isDraft: !this.state.isDraft });
-      return fetch("/api/feedbacks/save", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          feedback: {
-            receiverEmail: this.state.email,
-            giverEmail: this.props.sessionContext.user.email,
-            feedbackGood: "",
-            feedbackImprove: "",
-            feedbackAction: "",
-            isPending: true
-          }
-        })
-      }).then(response => {
-        let routerHistory = this.props.history;
-        if (response.ok) {
-          routerHistory.push("/");
-          return response.json().then(data => {
-            // const messages = [{ msg: data.msg }];
-            const pendingRequestId = data.pendingRequestId;
 
-<<<<<<< HEAD
-            feedbackProcess.sendRequestFeedbackEmail(
-              this.state.email,
-              this.props.messageContext,
-              this.props.sessionContext,
-              pendingRequestId
-            );
-          });
-        } else {
-          return response.json().then(json => {
-            if (json.msg === undefined) {
-              const messages = [
-                { msg: "Server error. Please try again later" }
-              ];
-              const identifier = "error";
-              setMessageWithTimeout(
-                this.props.messageContext,
-                messages,
-                TIMEOUTFOR,
-                identifier
-              );
-            } else {
-              const messages = [json];
-              const idenfitier = "error";
-              setMessageWithTimeout(
-                this.props.messageContext,
-                messages,
-                TIMEOUTFOR,
-                idenfitier
-              );
-            }
-          });
-        }
-      });
-    }
-=======
     await this.setState({ isDraft: !this.state.isDraft });
     return fetch("/api/feedbacks/save", {
       method: "post",
@@ -148,7 +84,6 @@ class RequestFeedback extends React.Component {
         });
       }
     });
->>>>>>> [Ben/Yamin][#67] Added NavigationPrompt and Bootstrap Modal
   }
 
   async handleChange(event) {
@@ -162,30 +97,6 @@ class RequestFeedback extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div style={{ marginTop: "20px" }} className="container">
-        <Messages messages={this.props.messageContext.messages} />
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <h1 style={{ display: "inline" }}>Request for Feedback</h1>
-          <button
-            style={{ display: "inline" }}
-            className="btn btn-success pull-right"
-            disabled={!this.state.isDraft}
-          >
-            Send
-          </button>
-          <div style={{ marginTop: "20px" }} className="form-group">
-            <label>Add Registered User Email address</label>
-            <input
-              style={{ display: "inline" }}
-              type="email"
-              name="email"
-              className="form-control"
-              id="emailAddress"
-              placeholder="Email"
-              onChange={this.handleChange.bind(this)}
-            />
-=======
       <React.Fragment>
         <div className="container">
           <form onSubmit={this.handleSubmit.bind(this)}>
@@ -224,7 +135,6 @@ class RequestFeedback extends React.Component {
                 }
               >
                 {({ isActive, onCancel, onConfirm }) => {
-                  console.log(isActive);
                   if (isActive) {
                     return (
                       <Modal show={this.state.show} onHide={this.handleClose}>
@@ -302,15 +212,10 @@ class RequestFeedback extends React.Component {
                 </button> */}
               </div>
             </div>
->>>>>>> [Ben/Yamin][#67] Added NavigationPrompt and Bootstrap Modal
           </div>
           <hr />
-          <Prompt
-            when={this.state.isDraft}
-            message="Your feedback hasn't been submitted. Are you sure you want to leave this page?"
-          />
-        </form>
-      </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
